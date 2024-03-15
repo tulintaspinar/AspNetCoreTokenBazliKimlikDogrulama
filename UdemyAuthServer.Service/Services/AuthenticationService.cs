90e8eff01788f1c140fd383c885451f6b7ae1612
tulintaspinar <tulinttaspinar@gmail.com>
2024-03-15 23:20:54 +0300

    client service added

---
+++ UdemyAuthServer.Service/Services/AuthenticationService.cs
@@ -0,0 +1,34 @@
+﻿using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyAuthServer.Core.Dtos;
+using UdemyAuthServer.Core.Services;
+
+namespace UdemyAuthServer.Service.Services
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        public Task<Response<TokenDto>> CreateByRefreshToken(string refreshToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<TokenDto>> CreateTokenAsync(LoginDto loginDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<ClientTokenDto>> CreateTokenByClient(ClientLoginDto clientLoginDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<NoDataDto>> RevokeRefreshToken(string refreshToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
